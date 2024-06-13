77dc5ea9b0278451acde1d9851f255fcfbad6bdd
Denis Kalinin <deniskalinin505@gmail.com>
2024-06-13 16:30:48 +0300

    Return module

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,11 +1,10 @@
 import sys
 import os
-from app import app_instance
-
 
 # add the app directory to the path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from app import app_instance
 
 if __name__ == "__main__":
     app_instance.run()
