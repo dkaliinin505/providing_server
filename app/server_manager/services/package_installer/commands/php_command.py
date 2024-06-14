5dc68f555a2121fe89cad499681e3bb06e5c0b69
Denis Kalinin <deniskalinin505@gmail.com>
2024-06-14 11:00:28 +0300

    Fixed file writer in php_command.py

--- app/server_manager/services/package_installer/commands/php_command.py
+++ app/server_manager/services/package_installer/commands/php_command.py
@@ -1,11 +1,9 @@
 import os
-
 from app.server_manager.interfaces.command_interface import Command
 from utils.util import run_command
 
 
 class PhpCommand(Command):
-
     def __init__(self, config):
         self.config = config
         print(f"Config: {config}")
@@ -65,31 +63,30 @@ class PhpCommand(Command):
 
         # Refresh PHP FPM
         run_command(
-            'echo "forge ALL=NOPASSWD: /usr/sbin/service php8.3-fpm reload" | sudo tee -a /etc/sudoers.d/php-fpm')
+            'echo "super_forge ALL=NOPASSWD: /usr/sbin/service php8.3-fpm reload" | sudo tee -a /etc/sudoers.d/php-fpm'
+        )
 
         # Set up session directory permissions
         run_command("sudo chmod 733 /var/lib/php/sessions")
         run_command("sudo chmod +t /var/lib/php/sessions")
 
         # Set up logrotate for PHP FPM
-        file_exists = run_command('test -f /etc/logrotate.d/php8.3-fpm && echo "exists"', return_json=False)
-        if file_exists.strip() == "exists":
-            grep_output = run_command(
-                'grep --count "maxsize" /etc/logrotate.d/php8.3-fpm', return_json=False, raise_exception=False
-            )
-            if grep_output is None or grep_output.strip() == "0":
-                with open('/etc/logrotate.d/php8.3-fpm', 'r') as file:
-                    lines = file.readlines()
-
-                with open('/etc/logrotate.d/php8.3-fpm', 'w') as file:
-                    for line in lines:
-                        file.write(line)
-                        if line.strip() in ['daily', 'weekly', 'monthly', 'yearly']:
-                            file.write('maxsize 100M\n')
-        else:
-            # Create logrotate file for PHP FPM
-            with open('/etc/logrotate.d/php8.3-fpm', 'w') as f:
-                f.write('daily\nmaxsize 100M\n')
+        logrotate_config = """
+        /var/log/php8.3-fpm.log {
+            daily
+            missingok
+            rotate 12
+            compress
+            delaycompress
+            notifempty
+            create 0640 root adm
+            sharedscripts
+            postrotate
+                [ -f /var/run/php/php8.3-fpm.pid ] && kill -USR1 `cat /var/run/php/php8.3-fpm.pid`
+            endscript
+        }
+        """
+        run_command(f'echo "{logrotate_config}" | sudo tee /etc/logrotate.d/php8.3-fpm')
 
         # Update PHP CLI to point to PHP 8.3
         run_command("sudo update-alternatives --set php /usr/bin/php8.3")
