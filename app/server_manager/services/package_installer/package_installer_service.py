d8577aa6ce0c8422fca680f702fe686c2c488998
Denis Kalinin <deniskalinin505@gmail.com>
2024-06-17 11:07:01 +0300

    Added redis install command

--- app/server_manager/services/package_installer/package_installer_service.py
+++ app/server_manager/services/package_installer/package_installer_service.py
@@ -1,6 +1,7 @@
 from app.server_manager.services.package_installer.commands.mysql_command import MySQLCommand
 from app.server_manager.services.package_installer.commands.nginx_command import NginxCommand
 from app.server_manager.services.package_installer.commands.php_command import PhpCommand
+from app.server_manager.services.package_installer.commands.redis_command import RedisCommand
 from app.server_manager.services.package_installer.invoker import PackageInstaller
 
 
@@ -10,6 +11,7 @@ class PackageInstallerService:
         self.installer.register('php', PhpCommand({'config': {}}))
         self.installer.register('nginx', NginxCommand({'config': {}}))
         self.installer.register('mysql', MySQLCommand({'config': {}}))
+        self.installer.register('redis', RedisCommand({'config': {}}))
 
     def install_package(self, data):
         package_name = data.get('package_name')
