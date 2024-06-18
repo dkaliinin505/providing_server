3983ac4cd29abcd9562c0b75c319cbfdd97c1fc3
Denis Kalinin <deniskalinin505@gmail.com>
2024-06-18 12:35:25 +0300

    Updated create_site_config_schema.py

--- app/server_manager/validators/schemas/management/create_site_config_schema.py
+++ app/server_manager/validators/schemas/management/create_site_config_schema.py
@@ -5,5 +5,4 @@ from app.server_manager.validators.rules.domain_rule import validate_domain
 
 # In the future, we will add some webroot and other rows here
 class CreateSiteSchema(Schema):
-    data = fields.Dict(required=True)
     domain = fields.Str(required=True, validate=validate_domain)
