d7df58f9bd99a34c12a8d7485dda4b708b0b26cc
Denis Kalinin <deniskalinin505@gmail.com>
2024-06-18 16:07:53 +0300

    Fixed generate_deploy_key_config_schema.py

--- app/server_manager/validators/rules/domain_exist_rule.py
+++ app/server_manager/validators/rules/domain_exist_rule.py
@@ -1,10 +1,9 @@
-import socket
+import os
 
 from marshmallow import ValidationError
 
 
-def validate_domain_existence(domain):
-    try:
-        socket.gethostbyname(domain)
-    except socket.error:
-        raise ValidationError(f"Domain '{domain}' does not exist.")
+def validate_domain_exist(domain):
+    nginx_conf_path = f"/etc/nginx/sites-available/{domain}"
+    if not os.path.isfile(nginx_conf_path):
+        raise ValidationError(f"Configuration file for domain '{domain}' does not exist.")
--- app/server_manager/validators/schemas/management/generate_deploy_key_config_schema.py
+++ app/server_manager/validators/schemas/management/generate_deploy_key_config_schema.py
@@ -1,8 +1,8 @@
 from marshmallow import Schema, fields
 
-from app.server_manager.validators.rules.domain_exist_rule import validate_domain_existence
+from app.server_manager.validators.rules.domain_exist_rule import validate_domain_exist
 from app.server_manager.validators.rules.domain_rule import validate_domain
 
 
 class GenerateDeployKeyCommandSchema(Schema):
-    domain = fields.Str(required=True, validate=[validate_domain, validate_domain_existence])
+    domain = fields.Str(required=True, validate=[validate_domain, validate_domain_exist])
