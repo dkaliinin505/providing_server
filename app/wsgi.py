0a63ae25ab9c6bf1fb8e6398dab23a38051486b6
Denis Kalinin <deniskalinin505@gmail.com>
2024-06-20 17:48:33 +0300

    Updated wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,10 +1,19 @@
 import sys
 import os
 
-# add the app directory to the path
+# Add the app directory to the path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import app_instance
+from utils.env_util import load_env
+
+
+# Load environment variables before the first request
+@app_instance.before_first_request
+def reload_env_vars():
+    load_env()
+
 
 if __name__ == "__main__":
+    load_env()  # Load environment variables before running the app
     app_instance.run()
