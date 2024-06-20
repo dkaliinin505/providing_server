bd6a8cdc4c0aa8b6d6ad5e2a332cf201a1752da8
Denis Kalinin <deniskalinin505@gmail.com>
2024-06-20 19:21:31 +0300

    Updated wsgi.py

--- app/wsgi.py
+++ app/wsgi.py
@@ -5,15 +5,6 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import app_instance
-from utils.env_util import load_env
-
-
-# Load environment variables before the first request
-@app_instance.before_first_request
-def reload_env_vars():
-    load_env()
-
 
 if __name__ == "__main__":
-    load_env()  # Load environment variables before running the app
     app_instance.run()
