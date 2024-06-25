741730b61c423bc098ace2ef2f6b03481f343c14
Denis Kalinin <deniskalinin505@gmail.com>
2024-06-25 19:20:12 +0300

    Updated delete_certbot_cert_command.py

--- app/server_manager/services/site_managment/commands/delete_certbot_cert_command.py
+++ app/server_manager/services/site_managment/commands/delete_certbot_cert_command.py
@@ -26,54 +26,34 @@ class DeleteCertBotCertCommand(Command):
         nginx_config_path = f"/etc/nginx/sites-available/{domain}"
         temp_config_path = f"/tmp/{domain}_nginx_config"
 
+        # Define the regex pattern to find the specific server block
+        pattern = re.compile(r"""
+                    server\s*\{\s*
+                    if\s*\(\$host\s*=\s*""" + re.escape(domain) + r"""\)\s*\{\s*
+                    return\s*301\s*https://\$host\$request_uri;\s*
+                    \}\s*
+                    server_name\s*""" + re.escape(domain) + r""";\s*
+                    listen\s*80;\s*
+                    \}\s*
+                    """, re.VERBOSE)
+
         with open(nginx_config_path, 'r') as file:
-            config_content = file.readlines()
+            config_content = file.read()
 
-        modified_content = []
-        inside_redirect_block = False
-        inside_server_block = False
-        brace_count = 0
-        redirect_block_lines = []
-
-        for line in config_content:
-            if "listen [::]:443 ssl ipv6only=on;" in line or "listen 443 ssl;" in line or \
-                    f"ssl_certificate /etc/letsencrypt/live/{domain}/fullchain.pem;" in line or \
-                    f"ssl_certificate_key /etc/letsencrypt/live/{domain}/privkey.pem;" in line or \
-                    "include /etc/letsencrypt/options-ssl-nginx.conf;" in line or "ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;" in line or \
-                    " managed by Certbot" in line:
-                continue
-
-            if re.search(r'server\s*{', line) and re.search(
-                    r'if\s*\(\$host\s*=\s*{}\)\s*{{\s*return\s*301\s*https://\$host\$request_uri;'.format(
-                        domain.replace('.', '\.')), line):
-                inside_redirect_block = True
-                brace_count = 1
-                redirect_block_lines.append(line)
-                continue
-
-            if inside_redirect_block:
-                brace_count += line.count('{') - line.count('}')
-                redirect_block_lines.append(line)
-                if brace_count == 0:
-                    inside_redirect_block = False
-                    continue
-
-            modified_content.append(line)
-
-        if redirect_block_lines:
-            modified_content = [
-                line for line in modified_content
-                if line not in redirect_block_lines
-            ]
-
-        # Print the number of substitutions made
-        logging.info(f"Number of lines in config: {len(config_content)}")
-
-        # Print config content after modification
-        logging.info("Modified Nginx Config Content:\n", "".join(modified_content))
+        # Remove all SSL-related lines
+        config_content = config_content.replace("listen [::]:443 ssl ipv6only=on;", "")
+        config_content = config_content.replace("listen 443 ssl;", "")
+        config_content = config_content.replace(f"ssl_certificate /etc/letsencrypt/live/{domain}/fullchain.pem;",
+                                                "")
+        config_content = config_content.replace(f"ssl_certificate_key /etc/letsencrypt/live/{domain}/privkey.pem;",
+                                                "")
+        config_content = config_content.replace("include /etc/letsencrypt/options-ssl-nginx.conf;", "")
+        config_content = config_content.replace("ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;", "")
+        config_content = config_content.replace("# managed by Certbot", "")
+        config_content = re.sub(pattern, '', config_content)
 
         with open(temp_config_path, 'w') as file:
-            file.writelines(modified_content)
+            file.write(config_content)
 
         shutil.move(temp_config_path, nginx_config_path)
         run_command("sudo systemctl reload nginx")
