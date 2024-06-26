a53fe99475bb1c094743dc893fc757b0c4e67463
Denis Kalinin <deniskalinin505@gmail.com>
2024-06-26 12:05:34 +0300

    Updated deploy_project_config_schema.py

--- app/server_manager/validators/schemas/site_management/deploy_project_config_schema.py
+++ app/server_manager/validators/schemas/site_management/deploy_project_config_schema.py
@@ -8,9 +8,9 @@ from app.server_manager.validators.rules.nested_structure_rule import validate_n
 class DeployProjectSchema(Schema):
     domain = fields.Str(required=True, validate=[validate_domain, validate_domain_exist])
     repository_url = fields.Url(required=True)
-    branch = fields.Str(required=True, validate=validate.Length(min=5, max=300))
+    branch = fields.Str(required=True, validate=validate.Length(min=1, max=300))
     is_nested_structure = fields.Bool(missing=False)
-    nested_folder = fields.Str(validate=validate.Length(min=3, max=300))
+    nested_folder = fields.Str(validate=validate.Length(min=2, max=300))
 
     @validates_schema
     def validate_nested_structure_wrapper(self, data, **kwargs):
