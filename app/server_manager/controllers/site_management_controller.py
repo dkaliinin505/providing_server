a86746f81acaa0be5ac45c0c36b8b072832d8f7d
Denis Kalinin <deniskalinin505@gmail.com>
2024-06-28 11:38:58 +0300

    Updated test method

--- app/server_manager/controllers/site_management_controller.py
+++ app/server_manager/controllers/site_management_controller.py
@@ -35,7 +35,7 @@ class SiteManagementController(Controller):
 
     @validate_request({'GET': RequestSchema})
     def test(self, data):
-        task_id = self.task_manager.submit_task(self.site_management_service.test(), data)
+        task_id = self.task_manager.submit_task(self.site_management_service.test, data)
         return jsonify({"message": "Test Task started in background", "task_id": task_id}), 200
 
     def __del__(self):
