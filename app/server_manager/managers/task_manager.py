d3339a89825172f70345e5566858a56f6bf652c7
Denis Kalinin <deniskalinin505@gmail.com>
2024-07-01 11:53:28 +0300

    Updated and added logging for task_manager.py

--- app/server_manager/managers/task_manager.py
+++ app/server_manager/managers/task_manager.py
@@ -1,17 +1,11 @@
-import concurrent.futures
 import asyncio
 import logging
+from concurrent.futures import ThreadPoolExecutor
 from threading import Lock
 from time import time
 
 
 class SingletonMeta(type):
-    """
-    The Singleton class can be implemented in different ways in Python. Some
-    possible methods include: base class, decorator, metaclass. We will use the
-    metaclass because it is best suited for this purpose.
-    """
-
     _instances = {}
     _lock: Lock = Lock()
 
@@ -25,7 +19,7 @@ class SingletonMeta(type):
 
 class TaskManager(metaclass=SingletonMeta):
     def __init__(self):
-        self.executor = concurrent.futures.ThreadPoolExecutor(max_workers=3)
+        self.executor = ThreadPoolExecutor(max_workers=3)
         self.loop = asyncio.get_event_loop()
         self.future_to_id = {}
         self.id_to_result = {}
@@ -37,16 +31,20 @@ class TaskManager(metaclass=SingletonMeta):
             self.id_counter += 1
             return self.id_counter
 
-    async def submit_task(self, func, *args):
+    def submit_task(self, func, *args, **kwargs):
         task_id = self._generate_unique_id()
-        future = self.loop.run_in_executor(self.executor, func, *args)
+        future = asyncio.run_coroutine_threadsafe(self._run_task(func, *args), self.loop)
         self.future_to_id[future] = task_id
         logging.info(f"Task submitted with ID: {task_id}")
         future.add_done_callback(self._task_done_callback)
         return task_id
 
+    async def _run_task(self, func, *args):
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(self.executor, func, *args)
+
     def _task_done_callback(self, future):
-        task_id = self.future_to_id.pop(future)
+        task_id = self.future_to_id.pop(future, None)
         logging.info(f"Task completed with ID: {task_id}")
         try:
             result = future.result()
@@ -56,11 +54,12 @@ class TaskManager(metaclass=SingletonMeta):
 
     def get_task_status(self, task_id):
         logging.info(f"Getting status for Task ID: {task_id}")
+        logging.info(f"Tasks: {self.future_to_id}")
+        logging.info(f"Results: {self.id_to_result}")
         if task_id in self.id_to_result:
             result, _ = self.id_to_result[task_id]
             return result
         for future, future_id in self.future_to_id.items():
-            logging.info(f"Checking future: {future}")
             if future_id == task_id:
                 if future.done():
                     try:
@@ -74,10 +73,3 @@ class TaskManager(metaclass=SingletonMeta):
                 else:
                     return {"task_id": task_id, "status": "in_progress"}
         return {"message": "Task ID not found"}
-
-    def cleanup(self):
-        logging.info("Shutting down executor")
-        self.executor.shutdown(wait=True)
-        logging.info("Executor shut down")
-
-
