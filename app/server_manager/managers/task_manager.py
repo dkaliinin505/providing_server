d2cb438796111923a5b6e8e69c9d7428ba37e5a9
Denis Kalinin <deniskalinin505@gmail.com>
2024-07-01 14:35:32 +0300

    Updated task_manager.py

--- app/server_manager/managers/task_manager.py
+++ app/server_manager/managers/task_manager.py
@@ -32,9 +32,9 @@ class TaskManager(metaclass=SingletonMeta):
             self.id_counter += 1
             return self.id_counter
 
-    async def submit_task(self, func, *args):
-        task_id = await self._generate_unique_id()
-        future = self.loop.run_in_executor(self.executor, func, *args)
+    async def submit_task(self, func, *args, **kwargs):
+        task_id = self._generate_unique_id()
+        future = self.executor.submit(func, *args, **kwargs)
         self.future_to_id[future] = task_id
         logging.info(f"Task submitted with ID: {task_id}")
         logging.info(f"Tasks: {self.future_to_id}")
