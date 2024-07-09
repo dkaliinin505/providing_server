153861d5ac741634affd6cf6ae191e2d5c197c2f
Denis Kalinin <deniskalinin505@gmail.com>
2024-07-09 15:11:48 +0300

    Updated db_util.py

--- app/server_manager/validators/schemas/server_management/mysql/create_database_schema.py
+++ app/server_manager/validators/schemas/server_management/mysql/create_database_schema.py
@@ -2,8 +2,8 @@ from marshmallow import Schema, fields
 
 
 class CreateDatabaseSchema(Schema):
-    db_name = fields.Str(required=True)
+    db_name = fields.Str(required=True, validate=fields.Length(min=1, max=32))
     create_user = fields.Bool(missing=False)
-    db_user = fields.Str()
-    db_user_password = fields.Str()
-    db_host = fields.IPv4(required=True, missing='%')
+    db_user = fields.Str(validate=fields.Length(min=6, max=32))
+    db_user_password = fields.Str(validate=fields.Length(min=6, max=32))
+    db_host = fields.IPv4(required=True)
--- app/server_manager/validators/schemas/server_management/mysql/create_database_user_schema.py
+++ app/server_manager/validators/schemas/server_management/mysql/create_database_user_schema.py
@@ -2,7 +2,7 @@ from marshmallow import Schema, fields
 
 
 class CreateDatabaseUserSchema(Schema):
-    db_user = fields.Str()
-    db_user_password = fields.Str()
-    db_privileges = fields.List(fields.Str(), missing=['ALL PRIVILEGES'])
-    db_host = fields.IPv4(required=True, missing='%')
+    db_user = fields.Str(validate=fields.Length(min=8, max=32))
+    db_user_password = fields.Str(validate=fields.Length(min=6, max=32))
+    db_privileges = fields.List(fields.Str(validate=fields.Length(min=6, max=32)), missing=['ALL PRIVILEGES'])
+    db_host = fields.IPv4(required=True)
