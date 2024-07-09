bd419cbe63717e44551e379d601e624524c0cfb9
Denis Kalinin <deniskalinin505@gmail.com>
2024-07-09 19:15:33 +0300

    Updated request_validator.py to handle serializable objects

--- app/server_manager/validators/request_validator.py
+++ app/server_manager/validators/request_validator.py
@@ -53,7 +53,7 @@ def validate_request(schema_classes):
 
                 errors = validate_data(validator, data)
                 if errors:
-                    return jsonify({'errors': errors}), 400
+                    return {'errors': errors}
 
                 if isinstance(validator, InstallPackageSchema):
                     package_name = data.get('package_name')
