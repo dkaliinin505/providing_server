12e57ef94429c62a0fe2d95c442d10c9cdbb07bc
Denis Kalinin <deniskalinin505@gmail.com>
2024-07-10 13:24:21 +0300

    Updated delete_database_user_command.py

--- app/server_manager/validators/schemas/server_management/mysql/create_database_user_schema.py
+++ app/server_manager/validators/schemas/server_management/mysql/create_database_user_schema.py
@@ -4,5 +4,5 @@ from marshmallow import Schema, fields, validate
 class CreateDatabaseUserSchema(Schema):
     db_user = fields.Str(validate=fields.Length(min=6, max=32))
     db_user_password = fields.Str(validate=fields.Length(min=6, max=32))
-    db_privileges = fields.List(fields.Str(validate=fields.Length(min=6, max=64)), missing=['ALL PRIVILEGES'])
+    db_privileges = fields.List(fields.Str(validate=fields.Length(min=3, max=64)), missing=['ALL PRIVILEGES'])
     db_name = fields.List(fields.Str(validate=validate.Length(min=1, max=64)), required=False)
