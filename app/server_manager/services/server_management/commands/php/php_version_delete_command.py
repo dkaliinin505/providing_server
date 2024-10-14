91a01dce7ae5c03a6a11e466dec0b7ca63d28a58
Denis Kalinin <deniskalinin505@gmail.com>
2024-10-14 18:43:21 +0300

    Added php_version_delete_command.py

--- app/server_manager/services/server_management/commands/php/php_version_delete_command.py
+++ app/server_manager/services/server_management/commands/php/php_version_delete_command.py
@@ -4,13 +4,13 @@ from utils.async_util import run_command_async
 
 class PhpVersionDeleteCommand(Command):
     def __init__(self, config):
+        self.php_version = '8.1'
         self.config = config
 
     async def is_default_version(self):
-        php_version = self.config.get('php_version', '8.1')
         default_version = await run_command_async('php -r "echo PHP_VERSION;"')
 
-        return default_version.startswith(php_version)
+        return default_version.startswith(self.php_version)
 
     async def count_installed_php_versions(self):
         result = await run_command_async("ls /etc/php/ | wc -l")
@@ -18,8 +18,10 @@ class PhpVersionDeleteCommand(Command):
         return int(result.strip())
 
     async def execute(self, data):
+        self.config = data
+        self.php_version = self.config.get('php_version', '8.1')
         if await self.is_default_version():
             return {"error": "You cannot delete the default PHP version."}
 
-        await run_command_async(f"sudo apt-get purge php{self.config.get('php_version', '8.1')}* -y")
-        return {"message": f"PHP {self.config.get('php_version', '8.1')} successfully deleted."}
+        await run_command_async(f"sudo apt-get purge php{self.php_version}* -y")
+        return {"message": f"PHP {self.php_version} successfully deleted."}
