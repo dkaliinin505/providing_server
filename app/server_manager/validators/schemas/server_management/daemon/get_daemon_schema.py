b5f16f5c5a45b935aeabf02d70ab8dc320787cbe
Denis Kalinin <deniskalinin505@gmail.com>
2024-10-16 18:49:48 +0300

    Updated get_daemon_log_command.py

--- app/server_manager/validators/schemas/server_management/daemon/get_daemon_schema.py
+++ app/server_manager/validators/schemas/server_management/daemon/get_daemon_schema.py
@@ -2,4 +2,5 @@ from marshmallow import Schema, fields, validate, ValidationError
 
 
 class GetDaemonSchema(Schema):
+    user = fields.Str(missing='super_forge', validate=validate.Length(min=1))
     daemon_id = fields.Str(required=True)
