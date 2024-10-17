84b38b71aed4b85142e381fd725f6533f41f9126
Denis Kalinin <deniskalinin505@gmail.com>
2024-10-17 16:59:00 +0300

    Updated clear_server_logs_command.py

--- app/server_manager/services/server_management/commands/logs/clear_server_logs_command.py
+++ app/server_manager/services/server_management/commands/logs/clear_server_logs_command.py
@@ -1,6 +1,11 @@
+import logging
+
 from utils.async_util import run_command_async
 import os
 from app.server_manager.interfaces.command_interface import Command
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 class ClearServerLogsCommand(Command):
@@ -20,15 +25,10 @@ class ClearServerLogsCommand(Command):
         try:
             command = f"sudo truncate -s 0 {self.log_path}"
             result, error_output = await run_command_async(command, capture_output=True)
-
-            if result:
-                return {
-                    "message": f"Log file at {self.log_path} cleared successfully."
-                }
-            else:
-                raise Exception({
-                    "message": f"Failed to clear logs: {error_output}"
-                })
+            logging.debug(f"Result: {result}")
+            return {
+                "message": f"Logs at {self.log_path} cleared successfully."
+            }
         except Exception as e:
             raise Exception({
                 "message": f"Failed to clear logs at {self.log_path}: {str(e)}"
