b01afa35e454419695da88d2d860cd70de634034
Denis Kalinin <deniskalinin505@gmail.com>
2024-10-31 16:44:13 +0200

    Updated control_daemon_schema.py

--- app/server_manager/validators/schemas/server_management/daemon/control_daemon_schema.py
+++ app/server_manager/validators/schemas/server_management/daemon/control_daemon_schema.py
@@ -2,6 +2,6 @@ from marshmallow import Schema, fields, validate, ValidationError
 
 
 class ControlDaemonSchema(Schema):
-    daemon_id = fields.Str(required=True),
+    daemon_id = fields.Str(required=True)
     num_processes = fields.Int(required=True, validate=validate.Range(min=1, max=100))
     action = fields.Str(required=True, validate=validate.OneOf(['start', 'stop', 'restart']))
