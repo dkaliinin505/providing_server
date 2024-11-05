3cfe90bcb0d407650de4ef5844a41d84ec7d44a5
Denis Kalinin <deniskalinin505@gmail.com>
2024-11-05 20:02:53 +0200

    Updated add_ssh_key_command.py

--- app/server_manager/services/server_management/commands/ssh_keys/add_ssh_key_command.py
+++ app/server_manager/services/server_management/commands/ssh_keys/add_ssh_key_command.py
@@ -4,6 +4,7 @@ import pwd
 import aiofiles
 import asyncio
 from app.server_manager.interfaces.command_interface import Command
+from utils.async_util import run_command_async
 
 
 class AddSSHKeyCommand(Command):
@@ -22,33 +23,16 @@ class AddSSHKeyCommand(Command):
         ssh_dir = f"/home/{self.user}/.ssh"
         authorized_keys_file = f"{ssh_dir}/authorized_keys"
 
-        await self._make_dirs_async(ssh_dir, mode=0o700)
-        await self._set_owner_and_permissions_async(ssh_dir, 0o700)
-
         key_with_id = f"# KEY_ID={self.key_id}\n{self.ssh_key}\n"
 
+        # Command to append the SSH key with the unique ID to the authorized_keys file
+        add_key_command = f'sudo echo "{key_with_id}" | tee -a {authorized_keys_file}'
+
+        # Execute the command
         try:
-            async with aiofiles.open(authorized_keys_file, 'a') as f:
-                await f.write(key_with_id)
+            await run_command_async(add_key_command, capture_output=True)
+            return {"message": f"SSH key added successfully", "key_id": self.key_id}
 
-            await self._set_owner_and_permissions_async(authorized_keys_file, 0o600)
-
-            return {
-                "message": "SSH key added successfully",
-                "data": {
-                    "id": self.key_id,
-                }
-            }
         except Exception as e:
-            return {"error": f"Failed to add SSH key: {str(e)}"}
 
-    async def _get_user_info_async(self):
-        return await asyncio.to_thread(pwd.getpwnam, self.user)
-
-    async def _make_dirs_async(self, path, mode=0o777):
-        await asyncio.to_thread(os.makedirs, path, mode, exist_ok=True)
-
-    async def _set_owner_and_permissions_async(self, path, mode):
-        user_info = await self._get_user_info_async()
-        await asyncio.to_thread(os.chmod, path, mode)
-        await asyncio.to_thread(os.chown, path, user_info.pw_uid, user_info.pw_gid)
+            raise Exception({"message": f"Failed to add SSH key: {str(e)}"})
