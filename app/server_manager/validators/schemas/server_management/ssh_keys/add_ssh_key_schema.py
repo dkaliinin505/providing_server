c3d9bb3e91062acbe2354eb7f073a5eb06667511
Denis Kalinin <deniskalinin505@gmail.com>
2024-11-05 20:17:09 +0200

    Updated add_ssh_key_schema.py

--- app/server_manager/validators/schemas/server_management/ssh_keys/add_ssh_key_schema.py
+++ app/server_manager/validators/schemas/server_management/ssh_keys/add_ssh_key_schema.py
@@ -1,7 +1,7 @@
 # self.user = self.config.get('user')
 #         self.ssh_key = self.config.get('ssh_key')
 from marshmallow import Schema, fields, validate, ValidationError, validates_schema, validates
-
+import pwd
 from app.server_manager.validators.rules.domain_rule import validate_domain
 from app.server_manager.validators.rules.nested_structure_rule import validate_nested_structure
 
@@ -11,9 +11,9 @@ class AddSSHKeySchema(Schema):
     user = fields.Str(required=True)
     ssh_key = fields.Str(required=True)
 
-    @validates('user')
-    def validate_user(self, value, pwd=None):
+    @validates_schema
+    def validate_user(self, data, **kwargs):
         try:
-            pwd.getpwnam(value)
+            pwd.getpwnam(data.get('user'))
         except KeyError:
-            raise ValidationError(f"User '{value}' does not exist.")
+            raise ValidationError(f"User '{data.get('user')}' does not exist.")
