4dcdef52984f7b52eca8bea58e4d9847b8f6522d
Denis Kalinin <deniskalinin505@gmail.com>
2024-12-09 12:58:53 +0200

    Updated run_scheduled_job_command.py

--- app/server_manager/services/server_management/commands/schedulers/run_scheduled_job_command.py
+++ app/server_manager/services/server_management/commands/schedulers/run_scheduled_job_command.py
@@ -14,6 +14,6 @@ class RunScheduledJobCommand(Command):
         result = await run_command_async(self.job_command, capture_output=True)
 
         if result:
-            return {"message": "Scheduled job executed successfully", "data": result}
+            return {"message": "Scheduled job executed successfully", "data": result.join()}
         else:
             return {"error": "Failed to execute scheduled job"}
