d461d1d91fa03a9cd48d29216e26a27809ad90d7
Denis Kalinin <deniskalinin505@gmail.com>
2024-12-09 10:56:59 +0200

    Updated imports.py

--- app/server_manager/imports.py
+++ app/server_manager/imports.py
@@ -48,3 +48,4 @@ from app.server_manager.validators.schemas.server_management.network.remove_fire
 from app.server_manager.validators.schemas.server_management.logs.server_logs_schema import ServerLogsSchema
 from app.server_manager.validators.schemas.server_management.ssh_keys.remove_ssh_key_schema import RemoveSSHKeySchema
 from app.server_manager.validators.schemas.server_management.ssh_keys.add_ssh_key_schema import AddSSHKeySchema
+from app.server_manager.validators.schemas.server_management.scheduler.control_scheduled_job_schema import ControlScheduledJobSchema
